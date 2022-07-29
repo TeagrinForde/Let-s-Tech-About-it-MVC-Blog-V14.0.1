f20a2344418fa0b66a889ae117ce3388b9890c53
Teagrin Forde <teagrinb@gmail.com>
2022-07-28 21:23:33 -0400

    Created content containers in handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,15 +1,6 @@
 module.exports = {
-  // Helper function returns a randomly generated book emoji
-  get_emoji: () => {
-    const randomNum = Math.random();
-    let book = "📗";
-
-    if (randomNum > 0.7) {
-      book = "📘";
-    } else if (randomNum > 0.4) {
-      book = "📙";
-    }
-
-    return `<span for="img" aria-label="book">${book}</span>`;
+  // Helper function returns the current data and time of blog post
+  current_time: (date) => {
+    return date.toLocaleTimeString();
   },
 };
