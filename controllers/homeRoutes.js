fee2d17255c420951962c2e2c4ddb9546d473a09
Teagrin Forde <teagrinb@gmail.com>
2022-08-02 18:03:33 -0400

    comment-routes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -31,6 +31,12 @@ router.get('/login', (req, res) => {
 });
 
 // Dashboard route
-
+router.get('/partials/dashboard', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('post');
+});
 
 module.exports = router;
