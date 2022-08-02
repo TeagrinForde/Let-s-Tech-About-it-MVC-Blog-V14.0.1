254aa7b49ef0c3564efc526891539cfb5f2b65e3
Teagrin Forde <teagrinb@gmail.com>
2022-08-02 09:56:53 -0400

    Add postID to comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -26,6 +26,14 @@ Comment.init(
         model: 'user',
         key: 'id',
         },
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'post',
+            key: 'id',
+        },
     },    
   },
   {
